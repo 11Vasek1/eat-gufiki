88d81ee093429a395032c75002bb898725cbe8e3
DmitryNavalov <dmitry.navalov@videinfra.com>
2023-09-27 05:01:08 +0300

    add canvas and add save state to local storage

--- src/Canvas/Canvas.tsx
+++ src/Canvas/Canvas.tsx
@@ -1,40 +1,79 @@
 import { useEffect, useRef } from 'react';
 import './canvas.scss';
 
+const sizes = [62, 194, 326, 458, 590];
+const SIZE = 652;
+
 type canvasProps = {
 	h: Array<number>;
 	v: Array<number>;
 	d: Array<number>;
-    width: number,
-    height: number,
 };
 
-function draw(obj: canvasProps, c: HTMLCanvasElement) {
-	console.log(obj, c);
-
+function draw(obj: canvasProps, c: HTMLCanvasElement, size: number) {
 	const ctx = c.getContext('2d');
+    const microOffset = size/100;
 
     if(ctx){
-        ctx.beginPath();
-        ctx.arc(50, 50, 50, 0, 2 * Math.PI);
-        ctx.fill();
+        ctx.fillStyle = '#00000000'
+        ctx.fillRect(0, 0, size, size);
+
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "#00000090"
+
+        obj.h.forEach((el, i)=>{
+            if(el){
+                const offset = (sizes[i]/SIZE) * size;
+
+                ctx.beginPath()
+                ctx.moveTo(microOffset,offset);
+                ctx.lineTo(size - microOffset,offset)
+                ctx.stroke()
+            }
+        })
+
+        obj.v.forEach((el, i)=>{
+            if(el){
+                const offset = (sizes[i]/SIZE) * size;
+
+                ctx.beginPath()
+                ctx.moveTo(offset,microOffset);
+                ctx.lineTo(offset, size - microOffset)
+                ctx.stroke()
+            }
+        })
+
+        if(obj.d[0]){
+            ctx.beginPath()
+            ctx.moveTo(microOffset,microOffset);
+            ctx.lineTo(size - microOffset, size - microOffset)
+            ctx.stroke()
+        }
+
+        if(obj.d[1]){
+            ctx.beginPath()
+            ctx.moveTo(size - microOffset,microOffset);
+            ctx.lineTo(microOffset, size - microOffset)
+            ctx.stroke()
+        }
     }
 }
 
-function Canvas({ h, v, d, width, height }: canvasProps) {
-	console.log(h, v, d);
+function Canvas({ h, v, d}: canvasProps) {
 	const canvasRef = useRef(null);
 
 	useEffect(() => {
-        const c = canvasRef.current;
-		console.log(typeof c);
+        const c:HTMLCanvasElement = canvasRef.current;
 
-		// if(canvasElement)
-		// console.log(canvasElement);
-		draw({ h, v, d, width, height }, c);
+        const width = c.clientWidth;
+
+        c.width = width;
+        c.height = width;
+
+		draw({ h, v, d}, c, width);
 	});
 
-	return <canvas className='canvas' width={width} height={height} ref={canvasRef}></canvas>;
+	return <canvas className='canvas' ref={canvasRef}></canvas>;
 }
 
 export default Canvas;
--- src/Cards/Cards.tsx
+++ src/Cards/Cards.tsx
@@ -1,51 +1,70 @@
 import './cards.scss';
 import Card from '../Card/Card.tsx';
 import json from '../recepcies.json';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Canvas from '../Canvas/Canvas.tsx';
+import {getData, setData} from '../utils/db.ts';
 
 function Cards() {
-	console.log(json);
+    
 	const startStatuses = Array(25).fill(false);
 	const [statuses, setStatuses] = useState(startStatuses);
-
-	console.log(statuses);
-
-    let widht = 0;
-    let height = 0;
 
 	const createCallback = (index: number) => {
 		return (value: boolean) => {
 			const newArr = [...statuses];
 			newArr[index] = value;
 			setStatuses(newArr);
+            setData(newArr)
 		};
 	};
 
-    const boxRef = useRef(null);
-
     useEffect(()=>{
-        const element:HTMLElement = boxRef.current
-        if(element){
-            widht = element.clientWidth;
-            height = widht;
+        const arr = getData();
+        if (arr.length !== 0) {
+            setStatuses(arr)
         }
-    })
+    }, [])
+
+    const hArr = [];
+    const vArr = [];
+    const dArr = [];
+
+    {
+        for (let i = 0; i < 25; i+=5) {
+            const sum = statuses[i]+statuses[i+1]+statuses[i+2]+statuses[i+3]+statuses[i+4];
+            if(sum === 5){hArr.push(1)}else{hArr.push(0)}
+        }
+    
+        for (let i = 0; i < 5; i++) {
+            const sum = statuses[i]+statuses[i+5]+statuses[i+10]+statuses[i+15]+statuses[i+20];
+            if(sum === 5){vArr.push(1)}else{vArr.push(0)}
+        }
+    
+        {
+            const sum = statuses[0]+statuses[6]+statuses[12]+statuses[18]+statuses[24];
+            if(sum === 5){dArr.push(1)}else{dArr.push(0)}
+        }
+    
+        {
+            const sum = statuses[4]+statuses[8]+statuses[12]+statuses[16]+statuses[20];
+            if(sum === 5){dArr.push(1)}else{dArr.push(0)}
+        }
+    }
 
 	const cardList = json.recepcies.map((info, index) => (
 		<Card
 			title={info.title}
 			description={info.description}
-			// index={index}
 			key={index}
 			callback={createCallback(index)}
 			isActive={statuses[index]}
 		/>
 	));
 	return (
-		<div className='cards' ref={boxRef}>
+		<div className='cards'>
 			{cardList}
-			<Canvas h={[0,1,0,0,0]} v={[0,0,0,0,0]} d={[0,0]} width={widht} height={height}/>
+			<Canvas h={hArr} v={vArr} d={dArr}/>
 		</div>
 	);
 }
