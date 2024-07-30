7c806c7c544947c8ee4301b736dcdc3ea54f26db
DmitryNavalov <dmitry.navalov@videinfra.com>
2024-07-30 23:49:14 +0300

    change description and make new line in MA KA RO NY

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -3,39 +3,24 @@ import './card.scss';
 
 type cardProps = {
 	title: string;
-	// index: number;
 	callback: (value: boolean) => void;
 	isActive: boolean;
 };
 
 function Card({ title, callback, isActive }: cardProps) {
-	// const [isOpen, setIsOpen] = useState(false);
-
-	// const close = () => {
-	// 	console.log('close');
-	// 	setIsOpen(false);
-	// };
-	// const open = () => {
-	// 	console.log('open');
-	// 	setIsOpen(true);
-	// };
+    const lines = title.split('\n').map(t => <h3 className='card__title sm-title'>{t}</h3>)
 
     return (
 			<div
 				className={`card ${isActive ? 'card_active' : ''}`}
-				// onClick={open}
 				onClick={(event: React.MouseEvent) => {
 					event.preventDefault();
 					callback(!isActive);
 				}}
-				// onContextMenu={(event: React.MouseEvent) => {
-				// 	event.preventDefault();
-				// 	callback(!isActive);
-				// }}
 			>
 				<div className='card__content'>
 					<img src='./placeholder.png' className='card__image' />
-					<h3 className='card__title sm-title'>{title}</h3>
+					{lines}
 				</div>
 			</div>
 		);
